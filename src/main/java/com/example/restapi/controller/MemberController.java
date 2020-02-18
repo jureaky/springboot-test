b04786ab1354ea6d0432a795db1544432a8b8755
juhan <juhan@lgcns.com>
2020-02-18 19:45:14 +0900

    Add CORS

--- src/main/java/com/example/restapi/controller/MemberController.java
+++ src/main/java/com/example/restapi/controller/MemberController.java
@@ -3,6 +3,9 @@ package com.example.restapi;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletResponse;
 
 import java.util.Date;
 
@@ -10,6 +13,12 @@ import java.util.Date;
 @RequestMapping("/member")
 public class MemberController {
 
+	@ModelAttribute
+	public void setResponseHeader(HttpServletResponse response) {
+		response.setHeader("Access-Control-Allow-Credentials", false);
+		response.setHeader("Access-Control-Allow-Methods", "POST");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+	}
 	@PostMapping("/test")
 	public Member memberTest() {
 		return new Member(0L, "Wickies", 20, "Jeju", new Date());
